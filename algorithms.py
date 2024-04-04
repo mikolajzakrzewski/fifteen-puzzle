590259abbec9effde4ef56c97d9ae30d745f4f67
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-04-04 20:42:45 +0200

    Modified the scenario when the algorithms do not find a solution to a puzzle.

--- algorithms.py
+++ algorithms.py
@@ -9,7 +9,6 @@ from queue import PriorityQueue
 
 
 def bfs(search_order, starting_board):
-    max_steps = 1000000
     start = time.time()
     queue = Queue()
     queue.put(starting_board)
@@ -17,8 +16,7 @@ def bfs(search_order, starting_board):
     visited_states_num = 1
     processed_states_num = 0
     max_reached_depth = 0
-    steps = 0
-    while not queue.empty() and steps < max_steps:
+    while not queue.empty():
         current_board = queue.get()
         processed_states_num += 1
         for direction in search_order:
@@ -38,14 +36,12 @@ def bfs(search_order, starting_board):
                     visited_layouts.add(tuple(new_board.layout.flatten()))
                     if len(new_board.moves) > max_reached_depth:
                         max_reached_depth = len(new_board.moves)
-        steps += 1
-    print("dupa")
+
     utils.write_to_file(None, visited_states_num, processed_states_num, max_reached_depth, time.time() - start)
     return None
 
 
 def dfs(search_order, starting_board):
-    max_steps = 10000000
     start = time.time()
     visited_states_num = 1
     processed_states_num = 0
@@ -55,9 +51,8 @@ def dfs(search_order, starting_board):
     visited_states = {}
 
     stack = [(starting_board, 0)]
-    steps = 0
 
-    while stack and steps < max_steps:
+    while stack:
         current_board, current_depth = stack.pop()
         processed_states_num += 1
         current_state = tuple(current_board.layout.flatten())
@@ -83,14 +78,12 @@ def dfs(search_order, starting_board):
                 visited_states_num += 1
                 if current_depth + 1 > max_reached_depth:
                     max_reached_depth = current_depth + 1
-        steps += 1
-    print("dupa")
+
     utils.write_to_file(None, visited_states_num, processed_states_num, max_reached_depth, time.time() - start)
     return None
 
 
 def a_star_manhattan(starting_board):
-    max_steps = 100000
     start = time.time()
     visited_states_num = 1
     processed_states_num = 0
@@ -102,8 +95,7 @@ def a_star_manhattan(starting_board):
          starting_board)
     )
     visited_layouts = set()
-    steps = 0
-    while not priority_queue.empty() and steps < max_steps:
+    while not priority_queue.empty():
         current_board = priority_queue.get()[1]
         processed_states_num += 1
         for direction in 'LRUD':
@@ -131,14 +123,12 @@ def a_star_manhattan(starting_board):
                     visited_layouts.add(tuple(new_board.layout.flatten()))
                     if len(new_board.moves) > max_reached_depth:
                         max_reached_depth = len(new_board.moves)
-        steps += 1
-    print("dupa")
+
     utils.write_to_file(None, visited_states_num, processed_states_num, max_reached_depth, time.time() - start)
     return None
 
 
 def a_star_hamming(starting_board):
-    max_steps = 100000
     start = time.time()
     visited_states_num = 1
     processed_states_num = 0
@@ -148,9 +138,8 @@ def a_star_hamming(starting_board):
                                                                            starting_board
                                                                            .expected_layout.flatten()), starting_board))
     visited_layouts = set()
-    steps = 0
 
-    while not priority_queue.empty() and steps < max_steps:
+    while not priority_queue.empty():
         current_board = priority_queue.get()[1]
         processed_states_num += 1
         for direction in 'LRUD':
@@ -177,7 +166,6 @@ def a_star_hamming(starting_board):
                     visited_states_num += 1
                     if len(new_board.moves) > max_reached_depth:
                         max_reached_depth = len(new_board.moves)
-        steps += 1
-    print("dupa")
+
     utils.write_to_file(None, visited_states_num, processed_states_num, max_reached_depth, time.time() - start)
     return None
